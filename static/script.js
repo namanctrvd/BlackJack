14baf2e86a2626c3a241cc1be611a88d85f2c3cf
Naman <naman19102@iiitnr.edu.in>
2022-05-29 19:56:41 +0530

    Made it more efficient and user score feature added
    - User's score is now showing in the website.
    - With every hit card value is adding in the score. - Separated landing page and game page in different html files.
    - Deleted letsPlay() funtion as it is not required anymore.

--- static/script.js
+++ static/script.js
@@ -1,17 +1,9 @@
-/* Function to change the front page to game area */
-    
-function letsPlay() {
-    document.getElementById('landing-page').remove()
-    var playDiv = document.getElementById('container-1')
-    
-    playDiv.innerHTML += '<div class="flex-blackjack-row-1"> <img src = "static/images/ui/name.png" alt = "" width = "440" height = "210" > </div > <div class="flex-blackjack-row-2"> <img src="static/images/ui/dealer.jpeg" alt="" width="450" height="238" > </div> <div class="flex-blackjack-row-3"> <div id="dealer-box"> <h2>Dealer: <span id="dealer-blackjack-result">0</span></h2> </div> <div id="your-box"> <h2>You: <span id="your-blackjack-result">0</span></h2> </div> </div> <div class="flex-blackjack-row-4"> <div> <table> <tr> <th>Wins</th> <th>Losses</th> <th>Draws</th> </tr> <tr> <td><span id="wins">0</span></td> <td><span id="losses">0</span></td> <td><span id="draws">0</span></td> </tr> </table ></div><div><button class="btn-lg btn-primary mr-2" id="blackjack-hit-btn" onclick="blackjackHit()" >Hit</button> <button class="btn-lg btn-warning mr-2" id="blackjack-Stand-btn" onclick="blackjackStand()">Stand</button> <button class="btn-lg btn-danger" id="blackjack-deal-btn" onclick="blackjackDeal()">Deal</button> </div> '
-}
-
 
 let blackjackGame = {
-    'you': { 'scoreSpan':'#your-blackjack-result', 'div': '#your-box', 'score': 0 },
+    'you': { 'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0 },
     'dealer': { 'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0 },
-    'cards': ['cardClubs2', 'cardClubs3', 'cardClubs4', 'cardClubs5', 'cardClubs6', 'cardClubs7', 'cardClubs8', 'cardClubs9', 'cardClubs10', 'cardClubsA', 'cardClubsJ', 'cardClubsQ', 'cardClubsK', 'cardDiamonds2', 'cardDiamonds3', 'cardDiamonds4', 'cardDiamonds5', 'cardDiamonds6', 'cardDiamonds7', 'cardDiamonds8', 'cardDiamonds9', 'cardDiamonds10', 'cardDiamondsA', 'cardDiamondsJ', 'cardDiamondsQ', 'cardDiamondsK', 'cardHearts2', 'cardHearts3', 'cardHearts4', 'cardHearts5', 'cardHearts6', 'cardHearts7', 'cardHearts8', 'cardHearts9', 'cardHearts10', 'cardHeartsA', 'cardHeartsJ', 'cardHeartsQ', 'cardHeartsK', 'cardSpades2', 'cardSpades3', 'cardSpades4', 'cardSpades5', 'cardSpades6', 'cardSpades7', 'cardSpades8', 'cardSpades9', 'cardSpades10', 'cardSpadesA', 'cardSpadesJ', 'cardSpadesQ', 'cardSpadesK']
+    'cards': ['cardClubs2', 'cardClubs3', 'cardClubs4', 'cardClubs5', 'cardClubs6', 'cardClubs7', 'cardClubs8', 'cardClubs9', 'cardClubs10', 'cardClubsA', 'cardClubsJ', 'cardClubsQ', 'cardClubsK', 'cardDiamonds2', 'cardDiamonds3', 'cardDiamonds4', 'cardDiamonds5', 'cardDiamonds6', 'cardDiamonds7', 'cardDiamonds8', 'cardDiamonds9', 'cardDiamonds10', 'cardDiamondsA', 'cardDiamondsJ', 'cardDiamondsQ', 'cardDiamondsK', 'cardHearts2', 'cardHearts3', 'cardHearts4', 'cardHearts5', 'cardHearts6', 'cardHearts7', 'cardHearts8', 'cardHearts9', 'cardHearts10', 'cardHeartsA', 'cardHeartsJ', 'cardHeartsQ', 'cardHeartsK', 'cardSpades2', 'cardSpades3', 'cardSpades4', 'cardSpades5', 'cardSpades6', 'cardSpades7', 'cardSpades8', 'cardSpades9', 'cardSpades10', 'cardSpadesA', 'cardSpadesJ', 'cardSpadesQ', 'cardSpadesK'],
+    'cardMap': { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': [1, 11] }
 }
 
 const YOU = blackjackGame['you']
@@ -22,9 +14,11 @@ const hitSound = new Audio('static/sounds/swish.m4a')
 function blackjackHit() {
     let card = randomCard()
     showCard(card, YOU)
+    updateScore(card, YOU)
+    showScore(YOU)
 }
 
-function randomCard(){
+function randomCard() {
     let randomIndex = Math.floor(Math.random() * 52)
     return blackjackGame['cards'][randomIndex]
 }
@@ -40,10 +34,20 @@ function blackjackDeal() {
     let yourImages = document.querySelector('#your-box').querySelectorAll('img')
     let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img')
 
-    for (let i=0; i<yourImages.length; i++ ){
+    for (let i = 0; i < yourImages.length; i++) {
         yourImages[i].remove();
     }
     for (let i = 0; i < dealerImages.length; i++) {
         dealerImages[i].remove();
     }
+}
+
+function updateScore(card, activePlayer) {
+    activePlayer['score'] += blackjackGame['cardMap'][card.slice(-1)]
+}
+
+function showScore(activePlayer) {
+    console.log(document.querySelector(activePlayer['scoreSpan']).innerHTML)
+    document.querySelector(activePlayer['scoreSpan']).innerHTML = activePlayer['score']
+    console.log(document.querySelector(activePlayer['scoreSpan']).innerHTML)
 }
